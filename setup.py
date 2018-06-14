0f6d3c3133522e76b2929db13c00bb9a60169546
raybellwaves <rjbell1987@gmail.com>
2018-06-14 15:58:46 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+
+DISTNAME = 'xskillscore'
+VERSION = '0.0.1'
+LICENSE = 'Apache'
+AUTHOR = 'Ray Bell'
+AUTHOR_EMAIL = 'rjbell1987@gmail.com'
+DESCRIPTION = "xskillscore"
+URL = 'https://github.com/pydata/xarray'
+INSTALL_REQUIRES = ['scipy']
+TESTS_REQUIRE = ['pytest']
+PYTHON_REQUIRE = '>=3.6'
+
+
+setup(name=DISTNAME,
+      version=VERSION,
+      license=LICENSE,
+      author=AUTHOR,      
+      author_email=AUTHOR_EMAIL,
+      description=DESCRIPTION,
+      url=URL,
+      packages=find_packages(),
+      install_requires=INSTALL_REQUIRES,
+      tests_require=TESTS_REQUIRE,
+      python_requires=PYTHON_REQUIRE)
+
