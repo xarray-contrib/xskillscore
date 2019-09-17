6a7b2a76f380c323e7ed34157e249358522d7ef3
Ray Bell <rayjognbell0@gmail.com>
2019-09-16 22:49:38 -0400

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 DISTNAME = 'xskillscore'
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 LICENSE = 'Apache'
 AUTHOR = 'Ray Bell'
 AUTHOR_EMAIL = 'rayjohnbell0@gmail.com'
@@ -15,6 +15,7 @@ INSTALL_REQUIRES = [
     'scipy',
     'properscoring',
     'numba'
+    'bottleneck'
 ]
 TESTS_REQUIRE = ['pytest']
 PYTHON_REQUIRE = '>=3.6'
