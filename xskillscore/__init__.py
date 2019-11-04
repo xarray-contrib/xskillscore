892e3e542ad34a5e47d9bea7f71885532f376ada
AS <aaron.spring@mpimet.mpg.de>
2019-11-04 14:30:12 +0100

    demo notebook shows metrics on map

--- xskillscore/__init__.py
+++ xskillscore/__init__.py
@@ -1,10 +1,21 @@
 # flake8: noqa
 from .core.accessor import XSkillScoreAccessor
-from .core.deterministic import (mae, mse, pearson_r, pearson_r_p_value, rmse,
-                                 spearman_r)
+from .core.deterministic import (
+    mad,
+    mae,
+    mape,
+    mse,
+    pearson_r,
+    pearson_r_p_value,
+    rmse,
+    smape,
+    spearman_r,
+    spearman_r_p_value,
+)
 from .core.probabilistic import xr_brier_score as brier_score
 from .core.probabilistic import xr_crps_ensemble as crps_ensemble
 from .core.probabilistic import xr_crps_gaussian as crps_gaussian
 from .core.probabilistic import xr_crps_quadrature as crps_quadrature
-from .core.probabilistic import \
-    xr_threshold_brier_score as threshold_brier_score
+from .core.probabilistic import (
+    xr_threshold_brier_score as threshold_brier_score,
+)
