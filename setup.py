c00410c2e71190bb01bccea3f8a7b76694ed9266
Ray Bell <rayjognbell0@gmail.com>
2019-11-10 20:00:45 -0500

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 DISTNAME = 'xskillscore'
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 LICENSE = 'Apache'
 AUTHOR = 'Ray Bell'
 AUTHOR_EMAIL = 'rayjohnbell0@gmail.com'
