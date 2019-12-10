1ad5243f493966267d649e4905f1ef90dd38867a
Riley Brady <riley.brady@colorado.edu>
2019-12-10 12:03:42 -0700

    add linter for CI

---
+++ ci/run-linter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+set -eo pipefail
+
+echo "Code Styling with (black and flake8)"
+
+source activate xskillscore-dev 
+
+echo "[flake8]"
+flake8 xskillscore --max-line-length=88 --exclude=__init__.py --ignore=W605,W503,C901
+
+echo "[black]"
+black --check --line-length=88 -S xskillscore 
