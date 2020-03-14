ccb223ff09ccc7c0689803e68b9fe8da41bdb749
ahuang11 <ahuang11@illinois.edu>
2020-03-14 11:35:59 -0500

    Fix assignment read only issue

---
+++ xskillscore/core/util.py
@@ -0,0 +1,21 @@
+# flake8: noqa
+from .core.deterministic import (
+    mad,
+    mae,
+    mape,
+    mse,
+    pearson_r,
+    pearson_r_p_value,
+    rmse,
+    smape,
+    spearman_r,
+    spearman_r_p_value,
+)
+from .core.probabilistic import xr_brier_score as brier_score
+from .core.probabilistic import xr_crps_ensemble as crps_ensemble
+from .core.probabilistic import xr_crps_gaussian as crps_gaussian
+from .core.probabilistic import xr_crps_quadrature as crps_quadrature
+from .core.probabilistic import (
+    xr_threshold_brier_score as threshold_brier_score,
+)
+
