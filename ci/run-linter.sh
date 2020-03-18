2ade3e50475c56916b0847e79e1b17279bc4dbd8
AS <aaron.spring@mpimet.mpg.de>
2020-03-18 17:43:04 +0100

    lint

--- ci/run-linter.sh
+++ ci/run-linter.sh
@@ -2,7 +2,7 @@
 set -e
 set -eo pipefail
 
-echo "Code Styling with (black, flake8 and isort)"
+echo "Code Styling with (black, flake8)"
 
 source activate xskillscore-dev
 
@@ -12,8 +12,8 @@ flake8 xskillscore --max-line-length=88 --exclude=__init__.py --ignore=W605,W503
 echo "[black]"
 black --check --line-length=88 -S xskillscore
 
-echo "[isort]"
-isort --recursive --check-only xskillscore
+#echo "[isort]"
+#isort --recursive --check-only xskillscore
 
 #echo "[doc8]"
 #doc8 docs/source
