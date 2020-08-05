ae7b556ce510811b7ddec95b5b927d7a889c7fbd
AS <aaron.spring@mpimet.mpg.de>
2020-08-05 10:48:54 +0200

    minor

--- xskillscore/tests/test_contingency.py
+++ xskillscore/tests/test_contingency.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pytest
 import xarray as xr
-from xskillscore import Contingency
 from sklearn.metrics import confusion_matrix
 
+from xskillscore import Contingency
 
+DIMS = [['time'], ['lon'], ['lat'], 'time', ['lon', 'lat', 'time']]
 
-DIMS = [['time'],['lon'],['lat'],'time',['lon','lat','time']]
 
 @pytest.fixture
 def forecast():
@@ -24,38 +24,44 @@ def observation(forecast):
     return b
 
 
-
 @pytest.fixture
 def category_edges_a():
-    return np.linspace(-2,2,5)
+    return np.linspace(-2, 2, 5)
+
 
 @pytest.fixture
 def category_edges_b():
-    return np.linspace(-3,3,5)
+    return np.linspace(-3, 3, 5)
 
 
-@pytest.mark.parametrize('type',['da','ds','chunked_da','chunked_ds'])
+@pytest.mark.parametrize('type', ['da', 'ds', 'chunked_da', 'chunked_ds'])
 @pytest.mark.parametrize('dim', DIMS)
-def test_Contingency_da(forecast,observation,category_edges_a,category_edges_b, dim, type):
+def test_Contingency_values(
+    forecast, observation, category_edges_a, category_edges_b, dim, type
+):
     if 'ds' in type:
-        name='var'
+        name = 'var'
         forecast = forecast.to_dataset(name=name)
         observation = observation.to_dataset(name=name)
     if 'chunked' in type:
         forecast = forecast.chunk()
         observation = observation.chunk()
-    cont_table = Contingency(forecast,observation,category_edges_a,category_edges_b, dim=dim)
+    cont_table = Contingency(
+        forecast, observation, category_edges_a, category_edges_b, dim=dim
+    )
     assert cont_table
     # test against sklearn.metrics.confusion_matrix
-    if type == 'da' and dim=='time':
+    if type == 'da' and dim == 'time':  # only tests for one dim
+
         def logical(ds):
-            return ds < .5
+            return ds < 0.5
 
         for lon in forecast.lon:
             for lat in forecast.lat:
-                forecast_1d = logical(forecast.sel(lon=lon,lat=lat))
-                observation_1d = logical(observation.sel(lon=lon,lat=lat))
+                forecast_1d = logical(forecast.sel(lon=lon, lat=lat))
+                observation_1d = logical(observation.sel(lon=lon, lat=lat))
                 sklearn_cont_table_1d = confusion_matrix(forecast_1d, observation_1d)
-                expected == cont_table.sel(lon=lon,lat=lat).values
-                print(int(lon),int(lat),sklearn_cont_table_1d, expected)
-                assert sklearn_cont_table_1d == expected
+                expected == cont_table.sel(lon=lon, lat=lat).values
+                assert sklearn_cont_table_1d == expected, print(
+                    int(lon), int(lat), sklearn_cont_table_1d, expected
+                )
