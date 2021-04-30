c60878613e1a8448ef40ca17bcf31e47dbbe8443
Aaron Spring <aaronspring@users.noreply.github.com>
2021-04-30 20:48:27 +0200

    Update test_probabilistic.py

--- xskillscore/tests/test_probabilistic.py
+++ xskillscore/tests/test_probabilistic.py
@@ -475,7 +475,7 @@ def test_rps_category_edges_None_fails(o, f_prob):
 
 def test_rps_category_edges_None_works(o, f_prob):
     """Test that rps expects inputs to have category_edges dim if category_edges is None."""
-    o = o.rename({"time": "category_edge"})
+    o = o.rename({"time": "category"})
     f_prob = f_prob.rename({"time": "category"}).mean("member")
     rps(o, f_prob, category_edges=None, dim=[])
 
