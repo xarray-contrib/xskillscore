66638c6da17d909b1cc398dbb8e976dedad3013a
Aaron Spring <aaronspring@users.noreply.github.com>
2021-11-18 23:21:53 +0100

    add comment _reliability dask

--- xskillscore/core/np_probabilistic.py
+++ xskillscore/core/np_probabilistic.py
@@ -9,6 +9,7 @@ def _reliability(o, f, bin_edges):
     """Return the reliability and number of samples per bin"""
     # I couldn't get dask='parallelized' working in this case
     # so dealing with dask arrays explicitly
+    # if true, imports dask.array later
     is_dask_array = not isinstance(o, np.ndarray) or not isinstance(f, np.ndarray)
 
     if is_dask_array:
