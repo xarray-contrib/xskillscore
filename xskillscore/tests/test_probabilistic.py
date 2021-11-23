b886b6ed246347476a862f7884e15c1072833f26
Aaron Spring <aaronspring@users.noreply.github.com>
2021-11-23 02:33:31 +0100

    failing test

--- xskillscore/tests/test_probabilistic.py
+++ xskillscore/tests/test_probabilistic.py
@@ -1086,3 +1086,12 @@ def test_brier_score_float_forecast_or_observations(o, f_prob):
     brier_score(o, f_prob)
     brier_score(o, 0.5)
     brier_score(1, f_prob)
+
+def test_rank_hist_tied():
+    """Test that rank_histogram handles tied ranks."""
+    a = xr.DataArray(np.zeros(100),dims='a')
+    b = xr.DataArray(np.zeros((100,10)),dims=['a','member'])
+    rh = rank_histogram(a, b)
+    assert rh.max() != 100
+    assert rh.min() > 3
+    assert rh.max() < 30
