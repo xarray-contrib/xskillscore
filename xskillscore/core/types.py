6d70916337d1ff67668625a65cf5308b4e3f9c69
AS <aaron.spring@mpimet.mpg.de>
2021-12-30 12:47:26 +0100

    refactor

--- xskillscore/core/types.py
+++ xskillscore/core/types.py
@@ -4,6 +4,7 @@ from typing import List, Union
 
 import xarray as xr
 
-XArray = xr.Dataset | xr.DataArray
+XArray = Union[xr.Dataset, xr.DataArray]
+# XArray = xr.Dataset | xr.DataArray # raises error during build: TypeError: unsupported operand type(s) for |: 'ABCMeta' and 'type'
 Dim = Union[str, List[str], None]
 # Dim = str | List[str] | None
