9c33716b9e29f8c5c766c3c23cafb772a024ec35
AS <aaron.spring@mpimet.mpg.de>
2021-12-30 12:39:27 +0100

    try XArray new

--- xskillscore/core/types.py
+++ xskillscore/core/types.py
@@ -4,7 +4,7 @@ from typing import List, Optional, Union
 
 import xarray as xr
 
-XArray = Union[xr.Dataset, xr.DataArray]
-# XArray = xr.Dataset | xr.DataArray
+#XArray = Union[xr.Dataset, xr.DataArray]
+XArray = xr.Dataset | xr.DataArray
 Dim = Optional[Union[str, List[str]]]
 # Dim = str | List[str] | None
