c6773391b870440adfa66fb62442e0f53e432f51
Aaron Spring <aaronspring@users.noreply.github.com>
2022-02-25 13:50:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ LONG_DESCRIPTION = """Metrics for verifying forecasts"""
 URL = "https://github.com/xarray-contrib/xskillscore"
 with open("requirements.txt") as f:
     INSTALL_REQUIRES = f.read().strip().split("\n")
-PYTHON_REQUIRE = ">=3.7"
+PYTHON_REQUIRE = ">=3.8"
 
 EXTRAS_REQUIRE = {
     "accel": ["numba>=0.52", "bottleneck"],
