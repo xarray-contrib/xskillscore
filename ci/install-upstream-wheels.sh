89b060d395aa42c7ff77890ced5a2563ad96b526
Aaron Spring <aaronspring@users.noreply.github.com>
2022-05-13 16:39:32 +0200

    [test-upstream]

--- ci/install-upstream-wheels.sh
+++ ci/install-upstream-wheels.sh
@@ -32,5 +32,4 @@ python -m pip install \
     git+https://github.com/Unidata/cftime \
     git+https://github.com/pydata/xarray  \
     git+https://github.com/pydata/bottleneck  \
-    git+https://github.com/xgcm/xhistogram \
-    git+https://github.com/numpy/numpy
+    git+https://github.com/xgcm/xhistogram
