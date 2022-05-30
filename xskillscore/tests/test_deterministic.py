734f2a0f04767d18622071e4e13f6850469de710
Aaron Spring <aaronspring@users.noreply.github.com>
2022-05-30 16:40:53 +0200

    Update test_deterministic.py

--- xskillscore/tests/test_deterministic.py
+++ xskillscore/tests/test_deterministic.py
@@ -371,3 +371,4 @@ def test_correlation_broadcasts(a, b, metrics):
         "eff" not in metric.__name__
     ):  # effective metrics require to be applied over time
         with pytest.raises(ValueError, match="ndex"):  # matching "indexes along dimension" and "cannot align objects with join='exact' where index/labels/sizes are not equal along these coordinates (dimensions)"
+            metric(a, b_changed_coords, dim="lat")
